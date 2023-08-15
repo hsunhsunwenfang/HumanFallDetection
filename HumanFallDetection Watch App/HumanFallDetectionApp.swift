e122c2466a5b3ae4d31f8b2b792482e4b4fddc22
Hsun <hsunwenfang@gmail.com>
2023-08-15 17:40:51 +0800

    Initial Commit

---
+++ HumanFallDetection Watch App/HumanFallDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  HumanFallDetectionApp.swift
+//  HumanFallDetection Watch App
+//
+//  Created by Hsunwen Fang on 2023/8/15.
+//
+
+import SwiftUI
+
+@main
+struct HumanFallDetection_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HumanFallDetection/HumanFallDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  HumanFallDetectionApp.swift
+//  HumanFallDetection
+//
+//  Created by Hsunwen Fang on 2023/8/15.
+//
+
+import SwiftUI
+
+@main
+struct HumanFallDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
